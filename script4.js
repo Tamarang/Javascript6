a6355bc4383f2c1c36e995a669e6318f191fac50
Jan Siepel <91777292+Tamarang@users.noreply.github.com>
2021-12-18 22:19:07 +0100

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,23 @@
+const makePizza = function() {
+    console.log("Make and prep the dough");
+    console.log("Put pizza sauce on the dough, spread it out");
+    console.log("Add cheese and mozzarella");
+    console.log("Bake the pizza");
+    console.log("Serve");
+}
+const makeSushi = function(){
+    console.log("Prep the rice and layout the nori on bamboo mat");
+    console.log("spread the rice on the nori");
+    console.log("Add fillings to the bottom of the nori");
+    console.log("Roll the Sushi Part Way");
+    console.log("Roll the Sushi the Rest of the Way.");
+    console.log("Move to Cutting Board");
+    console.log("Cut the Sushi");
+    console.log("Serve");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
